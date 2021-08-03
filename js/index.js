324c1770f2b403be2e05e7511443b715f516b0ba
ngasaraphael <66059003+ngasaraphael@users.noreply.github.com>
2021-08-03 02:51:00 +0200

    form validation

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
 // Toggle menu icon
 
-const toggle = document.querySelector('.menu');
-const navigation = document.querySelector('.navigation ul');
-const introAnimation = document.querySelector('.intro-animation');
-const allBtn = document.querySelector('.all-btn');
-const designBtn = document.querySelector('.design-btn');
-const fullStackBtn = document.querySelector('.full-stack-btn');
-const designProjects = document.querySelectorAll('.design-project');
-const fullstackProjects = document.querySelectorAll('.fullstack-project');
-const allProjects = document.querySelectorAll('.all-projects');
+let toggle = document.querySelector('.menu');
+let navigation = document.querySelector('.navigation ul');
+let introAnimation = document.querySelector('.intro-animation');
+let allBtn = document.querySelector('.all-btn');
+let designBtn = document.querySelector('.design-btn');
+let fullStackBtn = document.querySelector('.full-stack-btn');
+let designProjects = document.querySelectorAll('.design-project');
+let fullstackProjects = document.querySelectorAll('.fullstack-project');
+let allProjects = document.querySelectorAll('.all-projects');
+//contact form
+let contactForm = document.querySelector('#contactform');
+let formName = document.querySelector('#form-name');
+let formEmail = document.querySelector('#form-email');
+let formMessage = document.querySelector('#form-message');
+let formAlert = document.querySelector('.form-alert');
 
 toggle.addEventListener('click', () => {
   toggle.classList.toggle('active');
@@ -22,34 +28,80 @@ toggle.addEventListener('click', () => {
 
 //Job Category sorting
 //Design Jobs
-designBtn.addEventListener('click', () => {
-  fullstackProjects.forEach((fullstackProject) => {
-    fullstackProject.style.display = 'none';
-    designProjects.forEach((designProject) => {
-      designProject.style.display = 'block';
+if (designBtn) {
+  designBtn.addEventListener('click', () => {
+    fullstackProjects.forEach((fullstackProject) => {
+      fullstackProject.style.display = 'none';
+      designProjects.forEach((designProject) => {
+        designProject.style.display = 'block';
+      });
     });
   });
-});
+}
 
 //Full stack jobs
-fullStackBtn.addEventListener('click', () => {
-  designProjects.forEach((designProject) => {
-    designProject.style.display = 'none';
+if (fullStackBtn) {
+  fullStackBtn.addEventListener('click', () => {
+    designProjects.forEach((designProject) => {
+      designProject.style.display = 'none';
 
-    fullstackProjects.forEach((fullstackProject) => {
-      fullstackProject.style.display = 'block';
+      fullstackProjects.forEach((fullstackProject) => {
+        fullstackProject.style.display = 'block';
+      });
     });
   });
-});
+}
 
 //All jobs
-allBtn.addEventListener('click', () => {
-  allProjects.forEach((allProject) => {
-    allProject.style.display = 'block';
+if (allBtn) {
+  allBtn.addEventListener('click', () => {
+    allProjects.forEach((allProject) => {
+      allProject.style.display = 'block';
+    });
   });
-});
+}
 
 // copyeright date
 document
   .getElementById('copyright')
   .appendChild(document.createTextNode(new Date().getFullYear()));
+
+//Contact  Form Validation
+contactForm.addEventListener('submit', sendMessage);
+
+function sendMessage(e) {
+  e.preventDefault();
+  let nameValue = formName.value.trim();
+  let emailValue = formEmail.value.trim();
+  let messageValue = formMessage.value.trim();
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (!nameValue || !emailValue || !messageValue) {
+    formAlert.innerText = 'Please fill all input fields';
+    formAlert.classList.add('failed');
+    formName.value = formName.value.trim();
+    formEmail.value = formEmail.value.trim();
+    formMessage.value = formMessage.value.trim();
+  }
+
+  if (!emailValue.match(re)) {
+    formAlert.innerText = 'Please enter valid email';
+    formAlert.classList.add('failed');
+    formName.value = formName.value.trim();
+    formEmail.value = formEmail.value.trim();
+    formMessage.value = formMessage.value.trim();
+  }
+
+  if (nameValue && messageValue && emailValue.match(re)) {
+    formAlert.innerText = 'Thanks for reaching out I will get back to you ASAP';
+    formAlert.classList.add('success');
+    formName.value = '';
+    formEmail.value = '';
+    formMessage.value = '';
+  }
+
+  setTimeout(() => {
+    location.reload();
+  }, 5000);
+}
